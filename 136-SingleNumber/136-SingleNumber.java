44fae8b78904eb8e06bcecc19af2d545e95ea8c5
Manikandaprabu <65881102+mkpg@users.noreply.github.com>
2026-07-08 16:49:16 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 7/8/2026, 4:49:16 PM
+class Solution {
+    // static{
+    //     for(int i = 0; i<500;i++){
+    //         singleNumber(new int[]{});
+    //     }
+    // }
+    public static int singleNumber(int[] nums) {
+        int i = 0;
+        int x = 0;
+        while(i<nums.length){
+            x ^= nums[i];
+            i++;
+        }
+        return x;
+    }
+}
